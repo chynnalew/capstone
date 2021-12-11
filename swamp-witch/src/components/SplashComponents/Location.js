04e0254e203f8fc924f147c55ebcd27982dc3fa8
chynnalew <chynnalew@yahoo.com>
2021-12-11 14:45:46 -0800

    add grid styling to location component

--- swamp-witch/src/components/SplashComponents/Location.js
+++ swamp-witch/src/components/SplashComponents/Location.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import MapComponent from './MapComponent';
+import React from "react";
+import MapComponent from "./MapComponent";
 
 const Location = () => {
   return (
-    <div id='location'>
-      <h1 class='sectionHeader'>Current Shop</h1>
-      <div class='mapStyle'>
-        <MapComponent />
-      </div>
-      <div class='mapText'>
-        <h1>Anatomy Tattoo</h1>
+    <div id="location">
+      <h1 className="sectionHeader">Current Shop</h1>
+      <div className="container">
+        <div></div>
+        <div className="mapStyle">
+          <MapComponent />
+        </div>
+        <div className="mapText">
+          <a clasName='shopName' href='https://anatomytattoo.com/'>Anatomy Tattoo</a>
+          <p>2820 NE Sandy Blvd
+            <br />
+            Portland, OR 97232
+            <br />
+            (503) 231-1199
+          </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Location;
