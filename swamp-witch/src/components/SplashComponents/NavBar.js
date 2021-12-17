1e476b8b3cfc02167177fe6db490310b90a27653
chynnalew <chynnalew@yahoo.com>
2021-12-17 10:59:04 -0800

    WIP, scroll doesn't work when switching component views

--- swamp-witch/src/components/SplashComponents/NavBar.js
+++ swamp-witch/src/components/SplashComponents/NavBar.js
@@ -7,31 +7,31 @@ const NavBar = (props) => {
     <nav className='sticky'>
       <ul>
         <li>
-          <Link onClick={()=>props.onMainComponentsClick()} to='about' smooth={true} duration={1000}>ABOUT</Link>
+          <Link onClick={()=>props.onMainComponentsClick()} to='about' smooth={true} duration={1000} delay={100}>ABOUT</Link>
         </li>
         <li>
           <a >GALLERY</a>
           <span className="dropBottom"></span>
           <ul>
             <li>
-              <Link onClick={()=>props.onTattoosGalleryClick()} to='gallery' smooth={true} duration={500}>Tattoos</Link>
+              <Link onClick={()=>props.onTattoosGalleryClick()} to='gallery' smooth={true} duration={500} delay={100}>Tattoos</Link>
             </li>
             <li>
-              <Link onClick={()=>props.onFlashGalleryClick()} to='gallery' smooth={true} duration={500}>Flash</Link>
+              <Link onClick={()=>props.onFlashGalleryClick()} to='gallery' smooth={true} duration={500} delay={100}>Flash</Link>
             </li>
             <li>
-              <Link onClick={()=>props.onPetGalleryClick()} to='gallery' smooth={true} duration={500}>Pet Portraits</Link>
+              <Link onClick={()=>props.onPetGalleryClick()} to='gallery' smooth={true} duration={500} delay={100}>Pet Portraits</Link>
             </li>
           </ul>
         </li>
         <li>
-          <Link onClick={()=>props.onMainComponentsClick()} to='faq' smooth={true} duration={1000}>FAQ</Link>
+          <Link onClick={()=>props.onMainComponentsClick()} to='faq' smooth={true} duration={1000} delay={100}>FAQ</Link>
         </li>
         <li>
           <a href="https://swampwitchtattoos.bigcartel.com/">SHOP</a>
         </li>
         <li>
-          <Link onClick={()=>props.onContactClick()} to='contact' smooth={true} duration={1000}>CONTACT</Link>
+          <Link onClick={()=>props.onContactClick()} to='contact' smooth={true} duration={1000} delay={100}>CONTACT</Link>
         </li>
       </ul>
     </nav>
