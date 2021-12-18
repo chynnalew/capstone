a229eb04790184b906b77cade76adf0aed236cd9
chynnalew <chynnalew@yahoo.com>
2021-12-18 13:38:49 -0800

    add logo

--- swamp-witch/src/components/HomeComponents/NavBar/NavBar.js
+++ swamp-witch/src/components/HomeComponents/NavBar/NavBar.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";
 import PropTypes from "prop-types";
+import logo from "../../../assets/img/logo/sm-logo.JPG";
 
 const NavBar = (props) => {
-
   return (
+    <div className='navWrap'>
+          <HashLink smooth className="navLink logoWrap" to="/#mainHeader">
+            <img className="navBarLogo" src={logo} alt="swamp witch logo" />
+          </HashLink>
     <div id="navBar">
       <div className="bars">
-        <ion-icon name="menu-outline" onClick={()=>props.onSideBarClick()}></ion-icon>
+        <ion-icon
+          name="menu-outline"
+          onClick={() => props.onSideBarClick()}
+        ></ion-icon>
       </div>
       <div className="navMenu">
-        <HashLink smooth className="navLink" to="/#about" >
-        About
+        <HashLink smooth className="navLink" to="/#about">
+          About
         </HashLink>
         <HashLink smooth className="navLink" to="/gallery/#galleryTop">
           Gallery
-        </HashLink>
-        <HashLink smooth className="navLink" to="/#faq" >
-          FAQ
-        </HashLink>
-        <HashLink smooth className="navLink" to="/#mainHeader">
-          <h1>logo</h1>
         </HashLink>
         <a
           className="navLink"
@@ -29,21 +30,25 @@ const NavBar = (props) => {
           target="_blank"
         >
           Shop
-        </a>
-        <NavLink className="navLink" to="/contact" >
+          </a>
+        <div></div>
+          <HashLink smooth className="navLink" to="/#faq">
+          FAQ
+        </HashLink>
+        <NavLink className="navLink" to="/contact">
           Contact
         </NavLink>
-        <NavLink className="navLink" to="/policies" >
+        <NavLink className="navLink" to="/policies">
           Policies
         </NavLink>
       </div>
-    </div>
+      </div>
+      </div>
   );
 };
 
 NavBar.propTypes = {
   onSideBarClick: PropTypes.func,
-
-}
+};
 
 export default NavBar;
