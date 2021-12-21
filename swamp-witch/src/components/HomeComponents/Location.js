303544047eb5e9c6ae020bf128705269940ad09e
chynnalew <chynnalew@yahoo.com>
2021-12-21 11:12:10 -0800

    finish prelim scaling for midMedia splash page components

--- swamp-witch/src/components/HomeComponents/Location.js
+++ swamp-witch/src/components/HomeComponents/Location.js
@@ -3,7 +3,7 @@ import React from "react";
 const Location = () => {
   return (
     <div id="location">
-      <h1 className="sectionHeader">Current Shop</h1>
+      <h1 className="sectionHeader">Currently Located</h1>
       <div className="container">
         <div></div>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.0489510424572!2d-122.6388317486075!3d45.52922047899921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495a0c632c2862d%3A0x39175b36a225cce3!2sAnatomy%20Tattoo!5e0!3m2!1sen!2sus!4v1639262881358!5m2!1sen!2sus" loading="lazy"></iframe>
