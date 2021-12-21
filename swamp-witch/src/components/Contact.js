1765ac875e627def3e49ccd9f6e5f6cddb9a26ca
chynnalew <chynnalew@yahoo.com>
2021-12-21 09:39:43 -0800

    update small media sidebar styling

--- swamp-witch/src/components/Contact.js
+++ swamp-witch/src/components/Contact.js
@@ -2,87 +2,87 @@ import React from "react";
 import axios from "axios";
 
 const Contact = () => {
-  const formId = process.env.REACT_APP_FORM_ID;
-  const formSparkUrl = `https://submit-form.com/${formId}`;
+  // const formId = process.env.REACT_APP_FORM_ID;
+  // const formSparkUrl = `https://submit-form.com/${formId}`;
 
-  const [formState, setFormState] = React.useState();
+  // const [formState, setFormState] = React.useState();
 
-  const inputFormControl = (event) => {
-    const { id, value } = event.target;
-    const formKey = id;
-    const updatedFormState = { ...formState };
-    updatedFormState[formKey] = value;
-    setFormState(updatedFormState);
-  };
+  // const inputFormControl = (event) => {
+  //   const { id, value } = event.target;
+  //   const formKey = id;
+  //   const updatedFormState = { ...formState };
+  //   updatedFormState[formKey] = value;
+  //   setFormState(updatedFormState);
+  // };
 
-  const submitForm = async (event) => {
-    event.preventDefault();
-    await postSubmission();
-  };
+  // const submitForm = async (event) => {
+  //   event.preventDefault();
+  //   await postSubmission();
+  // };
 
-  const postSubmission = async () => {
-    const payload = {
-      name: formState.name,
-      email: formState.email,
-      description: formState.description,
-      placement: formState.placement,
-      size: formState.size,
-      color: formState.color,
-      budget: formState.budget,
-    };
-    try {
-      const result = await axios.post(formSparkUrl, payload);
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div id="contact">
-      <h1 className="sectionHeader">Contact</h1>
-      <p>work in progress</p>
-      <form id="contact-form" onSubmit={submitForm}>
-        <input
-          onChange={inputFormControl}
-          type="text"
-          id="name"
-          placeholder="Your Name"
-          required="required"
-        />
-        <br />
-        <input
-          onChange={inputFormControl}
-          type="email"
-          id="email"
-          placeholder="Your Email"
-          required="required"
-        />
-        <br />
-        <textarea
-          onChange={inputFormControl}
-          id="description"
-          placeholder="detailed tattoo description"
-          required="required"
-        />
-        <br />
-
-        <input onChange={inputFormControl} id="placement" placeholder="placement" required="required" />
-        <br />
-        <input onChange={inputFormControl} id="size" placeholder="size" required="required" />
-        <br />
-        <input onChange={inputFormControl} id="color" placeholder="color" required="required" />
-        <br />
-        <input onChange={inputFormControl} id="budget" placeholder="budget" required="required" />
-        <br />
-        <input type="submit" value="Send" />
-      </form>
-    </div>
-  );
+  // const postSubmission = async () => {
+  //   const payload = {
+  //     name: formState.name,
+  //     email: formState.email,
+  //     description: formState.description,
+  //     placement: formState.placement,
+  //     size: formState.size,
+  //     color: formState.color,
+  //     budget: formState.budget,
+  //   };
+  //   try {
+  //     const result = await axios.post(formSparkUrl, payload);
+  //     console.log(result);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   // return (
   //   <div id="contact">
   //     <h1 className="sectionHeader">Contact</h1>
   //     <p>work in progress</p>
-  //     </div>)
+  //     <form id="contact-form" onSubmit={submitForm}>
+  //       <input
+  //         onChange={inputFormControl}
+  //         type="text"
+  //         id="name"
+  //         placeholder="Your Name"
+  //         required="required"
+  //       />
+  //       <br />
+  //       <input
+  //         onChange={inputFormControl}
+  //         type="email"
+  //         id="email"
+  //         placeholder="Your Email"
+  //         required="required"
+  //       />
+  //       <br />
+  //       <textarea
+  //         onChange={inputFormControl}
+  //         id="description"
+  //         placeholder="detailed tattoo description"
+  //         required="required"
+  //       />
+  //       <br />
+
+  //       <input onChange={inputFormControl} id="placement" placeholder="placement" required="required" />
+  //       <br />
+  //       <input onChange={inputFormControl} id="size" placeholder="size" required="required" />
+  //       <br />
+  //       <input onChange={inputFormControl} id="color" placeholder="color" required="required" />
+  //       <br />
+  //       <input onChange={inputFormControl} id="budget" placeholder="budget" required="required" />
+  //       <br />
+  //       <input type="submit" value="Send" />
+  //     </form>
+  //   </div>
+  // );
+  return (
+    <div id="contact">
+      <h1 className="sectionHeader">Contact</h1>
+      <p>work in progress</p>
+      </div>)
 };
 
 export default Contact;
--- swamp-witch/src/components/HomeComponents/Location.js
+++ swamp-witch/src/components/HomeComponents/Location.js
@@ -7,8 +7,9 @@ const Location = () => {
       <div className="container">
         <div></div>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.0489510424572!2d-122.6388317486075!3d45.52922047899921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495a0c632c2862d%3A0x39175b36a225cce3!2sAnatomy%20Tattoo!5e0!3m2!1sen!2sus!4v1639262881358!5m2!1sen!2sus" loading="lazy"></iframe>
+       <br/><br/>
         <div className="mapText">
-          <a clasName='shopName' href='https://anatomytattoo.com/'>Anatomy Tattoo</a>
+          <a clasName='shopName' href='https://anatomytattoo.com/'><strong>Anatomy Tattoo</strong></a>
           <p>2820 NE Sandy Blvd
             <br />
             Portland, OR 97232
