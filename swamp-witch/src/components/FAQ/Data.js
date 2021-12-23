9a5dbc8aa6ea1592b3b58c34fc8ea0402f759ee8
chynnalew <chynnalew@yahoo.com>
2021-12-23 10:54:42 -0800

    update FAQ

--- swamp-witch/src/components/FAQ/Data.js
+++ swamp-witch/src/components/FAQ/Data.js
@@ -1,22 +1,34 @@
 export const Data = [
   {
-    question: 'Question 1',
+    question: 'How much do you charge?',
     answer: "This is the answer for q1"
   },
   {
-    question: 'Question 2',
+    question: 'How do I book an appointment?',
     answer: "This is the answer for q2"
   },
   {
-    question: 'Question 3',
+    question: 'It\'s my first tattoo... What do I need to know?',
     answer: "This is the answer for q3"
   },
   {
-    question: 'Question 4',
+    question: 'How do I decide on a design?',
     answer: "This is the answer for q4"
   },
   {
-    question: 'Question 5',
+    question: 'What forms of payment do you accept?',
     answer: "This is the answer for q5"
   },
+  {
+    question: 'How do I prepare for my appointment?',
+    answer: "This is the answer for q6"
+  },
+  {
+    question: 'How do I take care of my new tattoo?',
+    answer: "This is the answer for q7"
+  },
+  {
+    question: 'How long does it take for my tattoo to heal?',
+    answer: "This is the answer for q8"
+  }
 ]
